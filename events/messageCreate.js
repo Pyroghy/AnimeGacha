bd6124d9cdfcc3717782573899e549d5b53573f5
Pyroghy <73313427+Pyroghy@users.noreply.github.com>
2022-01-09 14:45:56 -0500

    Update messageCreate.js

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const chalk = require('chalk');
 
 module.exports = async(bot, message) => {
     const profileModel = mongoose.model('Profiles');
-    const user = await profileModel.findOne({ Id: message.member.id });
+    const member = message.mentions.members.first() || message.member;
+    const user = await profileModel.findOne({ id: message.member.id });
     const prefix = process.env.PREFIX;
     const args = message.content.slice(prefix.length).trim().split(' ');
     const commandName = args.shift().toLowerCase();
@@ -11,18 +13,39 @@ module.exports = async(bot, message) => {
     if(!message.content.startsWith(prefix) || message.author.bot) { return }
     if(message.channel.type === 'dm') { return }
     if(!user) {
-        profileModel.create({
-            id: message.member.id,
+        await profileModel.create({ 
+            id: member.id, 
             guilds: [
                 { 
                     guild: message.guild.id, 
                     character: "None set",
                     color: "2f3136",
-                    image:message.member.user.avatarURL()
+                    image: member.user.avatarURL() 
                 }
             ], 
             badges: [] 
-        })
+        }).then(console.log(`${chalk.bold(message.member.id)} now has a profile`))
     }
-	if(command) { command.run(bot, message, args) }
+    else {
+        const findGuild = await profileModel.findOne({ id: member.id, 'guilds.guild': message.guild.id })
+
+        if(!findGuild) {
+            await profileModel.updateOne({ id: member.id },
+            { 
+                $push: { 
+                    guilds: [
+                        { 
+                            guild: message.guild.id, 
+                            character: "None set",
+                            color: "2f3136",
+                            image: member.user.avatarURL() 
+                        }
+                    ]
+                } 
+            }).then(console.log(chalk.green(`[${chalk.white.bold(message.guild.id)}] ${chalk.bold(findGuild.id)} is now registered`)))
+        }
+    }
+	if(command) { 
+        command.run(bot, message, args)
+    }
 };
